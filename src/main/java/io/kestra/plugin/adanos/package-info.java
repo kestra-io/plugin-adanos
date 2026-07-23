08316dc19ef36b189459e4f94df506a0690844ec
François Delbrayelle <fdelbrayelle@kestra.io>
2026-07-23 17:27:15 +0200

    chore: initial scaffold cleanup for plugin-adanos

---
+++ src/main/java/io/kestra/plugin/adanos/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Adanos",
+    description = "Adanos plugin for Kestra",
+    categories = PluginSubGroup.PluginCategory.DATA
+)
+package io.kestra.plugin.adanos;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
